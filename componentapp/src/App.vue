5414f857f7265f5e738470c51aa1982699708693
sweet-boyw <wangxj848@qq.com>
2021-12-02 16:29:05 +0800

    删除模板代码

--- componentapp/src/App.vue
+++ componentapp/src/App.vue
@@ -1,9 +1,13 @@
+<!--
+ * @Author: your name
+ * @Date: 2021-12-02 13:40:05
+ * @LastEditTime: 2021-12-02 13:56:18
+ * @LastEditors: Please set LastEditors
+ * @Description: 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
+ * @FilePath: \componentapp\componentapp\src\App.vue
+-->
 <template>
   <div id="app">
-    <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
-    </div>
     <router-view/>
   </div>
 </template>
@@ -13,20 +17,6 @@
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
-  text-align: center;
   color: #2c3e50;
-}
-
-#nav {
-  padding: 30px;
-
-  a {
-    font-weight: bold;
-    color: #2c3e50;
-
-    &.router-link-exact-active {
-      color: #42b983;
-    }
-  }
 }
 </style>
--- componentapp/src/views/Home.vue
+++ componentapp/src/views/Home.vue
@@ -1,18 +1,20 @@
+<!--
+ * @Author: your name
+ * @Date: 2021-12-02 13:40:05
+ * @LastEditTime: 2021-12-02 13:49:51
+ * @LastEditors: your name
+ * @Description: 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
+ * @FilePath: \componentapp\componentapp\src\views\Home.vue
+-->
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    hello world
   </div>
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
 
 export default {
   name: 'Home',
-  components: {
-    HelloWorld
-  }
 }
 </script>
